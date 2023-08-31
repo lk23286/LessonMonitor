a85d5dd1930c938a3e51d98ddd0872903e2341db
lk23286 <lk23286@gmail.com>
2023-08-31 18:50:33 +0200

    Initial Commit

---
+++ LessonMonitor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LessonMonitor
+//
+//  Created by Laszlo Kovacs on 2023. 08. 31..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
